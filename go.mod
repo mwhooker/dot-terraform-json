f83b59c607d1afdf73018adae6d26c2bca4ab3b8
Matthew Hooker <mwhooker@gmail.com>
2021-05-18 11:15:29 -0700

    support state

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/awalterschulze/gographviz v2.0.3+incompatible
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/hashicorp/go-terraform-address v0.0.0-20210517223925-9193ee43dc40 // indirect
 	github.com/hashicorp/terraform-json v0.10.0
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -23,8 +24,51 @@ func newPlan(planInput io.Reader) (*tfjson.Plan, error) {
 	return parsed, nil
 }
 
+func newState(planInput io.Reader) (*tfjson.State, error) {
+	parsed := &tfjson.State{}
+
+	dec := json.NewDecoder(planInput)
+	dec.DisallowUnknownFields()
+	if err := dec.Decode(parsed); err != nil {
+		return nil, err
+	}
+
+	return parsed, nil
+}
+
 func usage() {
-	fmt.Fprint(os.Stderr, "usage: tf2json <plan.json>")
+	fmt.Fprint(os.Stderr, "usage: tf2json <plan.json|state.json>")
+}
+
+func openPlanOrState(fName string) (interface{}, error) {
+	f, err := os.Open(fName)
+	if err != nil {
+		return nil, fmt.Errorf("could not file file: %w", err)
+	}
+
+	plan, err := newPlan(f)
+	if err != nil {
+		return nil, fmt.Errorf("error making plan: %w", err)
+	}
+
+	if plan.PlannedValues != nil && plan.PriorState != nil && plan.Config != nil {
+		return plan, nil
+	}
+
+	if _, err := f.Seek(0, 0); err != nil {
+		return nil, err
+	}
+
+	state, err := newState(f)
+	if err != nil {
+		return nil, fmt.Errorf("error making state: %w", err)
+	}
+
+	if state.Values != nil {
+		return state, nil
+	}
+
+	return nil, nil
 }
 
 func main() {
@@ -32,20 +76,17 @@ func main() {
 		usage()
 		os.Exit(1)
 	}
-	planFile := os.Args[1]
+	inFile := os.Args[1]
 
-	err := realMain(planFile)
+	err := realMain(inFile)
 	if err != nil {
 		fmt.Fprint(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 }
 
-func realMain(planFile string) error {
-
-	f, err := os.Open(planFile)
-
-	plan, err := newPlan(f)
+func realMain(inFile string) error {
+	i, err := openPlanOrState(inFile)
 	if err != nil {
 		return err
 	}
@@ -58,55 +99,80 @@ func realMain(planFile string) error {
 	graph.AddAttr("G", "newrank", "true")
 	graph.AddAttr("G", "compoun", "true")
 
-	var walk func(string, *tfjson.StateModule) string
+	gv := Graph{graph}
 
-	walk = func(graphName string, m *tfjson.StateModule) string {
-		var maddr string
-		if m.Address == "" {
-			maddr = graphName
-		} else {
-			maddr = fmt.Sprintf("%s: %s", graphName, m.Address)
+	switch ps := i.(type) {
+	case *tfjson.Plan:
+		if err := gv.Plan(ps); err != nil {
+			return err
 		}
-		// add the module
-		graph.AddNode(graphName, maddr, nil)
-		for _, r := range m.Resources {
-			a, err := address.NewAddress(r.Address)
-			if err != nil {
-				panic(err)
-			}
-
-			label := map[string]string{
-				"label": a.ResourceSpec.String(),
-				"shape": "box",
-			}
-
-			rName := fmt.Sprintf("%s.%s", maddr, a.ResourceSpec.String())
-
-			if a.Mode == address.DataResourceMode {
-				label["color"] = "green"
-			} else {
-				label["color"] = "blue"
-			}
-
-			graph.AddNode(graphName, rName, label)
-			graph.AddEdge(maddr, rName, true, nil)
+	case *tfjson.State:
+		if err := gv.State(ps); err != nil {
+			return err
 		}
-		for _, c := range m.ChildModules {
-			p := walk(graphName, c)
-			graph.AddEdge(maddr, p, true, nil)
-		}
-		return maddr
+	default:
+		return errors.New("couldn't detect file type")
 	}
-
-	// TODO: support state output
-
-	graph.AddSubGraph("G", "planned", nil)
-	graph.AddSubGraph("G", "prior", nil)
-	walk("planned", plan.PlannedValues.RootModule)
-	walk("prior", plan.PriorState.Values.RootModule)
 
 	output := graph.String()
 	fmt.Println(output)
+	return nil
+}
+
+func (g *Graph) State(state *tfjson.State) error {
+	g.gv.AddSubGraph("G", "state", nil)
+	g.Walk("state", state.Values.RootModule)
 
 	return nil
+}
+
+func (g *Graph) Plan(plan *tfjson.Plan) error {
+	g.gv.AddSubGraph("G", "planned", nil)
+	g.gv.AddSubGraph("G", "prior", nil)
+	g.Walk("planned", plan.PlannedValues.RootModule)
+	g.Walk("prior", plan.PriorState.Values.RootModule)
+
+	return nil
+}
+
+type Graph struct {
+	gv gographviz.Interface
+}
+
+func (g *Graph) Walk(graphName string, m *tfjson.StateModule) string {
+	var maddr string
+	if m.Address == "" {
+		maddr = graphName
+	} else {
+		maddr = fmt.Sprintf("%s: %s", graphName, m.Address)
+	}
+	// add the module
+	g.gv.AddNode(graphName, maddr, nil)
+	for _, r := range m.Resources {
+		a, err := address.NewAddress(r.Address)
+		if err != nil {
+			panic(err)
+		}
+
+		label := map[string]string{
+			"label": a.ResourceSpec.String(),
+			"shape": "box",
+		}
+
+		rName := fmt.Sprintf("%s.%s", maddr, a.ResourceSpec.String())
+
+		if a.Mode == address.DataResourceMode {
+			label["color"] = "green"
+		} else {
+			label["color"] = "blue"
+		}
+
+		g.gv.AddNode(graphName, rName, label)
+		g.gv.AddEdge(maddr, rName, true, nil)
+	}
+	for _, c := range m.ChildModules {
+		p := g.Walk(graphName, c)
+		g.gv.AddEdge(maddr, p, true, nil)
+	}
+	return maddr
 }
